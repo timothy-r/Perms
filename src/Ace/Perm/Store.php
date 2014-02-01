bd7be1f994c92d946fb817131e43da8ef93e54e6
Tim Rodger <tim.rodger@gmail.com>
2014-02-01 21:41:15 +0000

    Adds Store implementation

---
+++ src/Ace/Perm/Store.php
@@ -0,0 +1,13 @@
+<?php namespace Ace\Perm;
+
+use Ace\Perm\StoreInterface;
+use Ace\Perm\SubjectInterface;
+use Ace\Perm\ObjectInterface;
+
+class Store implements StoreInterface
+{
+    public function get(SubjectInterface $subject, ObjectInterface $object)
+    {
+
+    }
+}
