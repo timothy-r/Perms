d969bc191accc4a652adcb1e7f1d0833ccbf39da
Tim Rodger <tim.rodger@gmail.com>
2014-02-02 10:21:56 +0000

    Adds returning a Perm object from Store::get

--- app.php
+++ app.php
@@ -1,7 +1,10 @@
 <?php
 use Silex\Application;
+use Ace\Perm\Store;
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+$store = new Store;
 
 $app = new Application;
 $app['debug'] = true;
@@ -11,13 +14,14 @@ $app->get('/', function(Application $app) {
     return $app->json($data);
 });
 
-$app->get('/{subject_type}/{subject_id}/{object_type}/{object_id}', function(Application $app, $subject_type, $subject_id, $object_type, $object_id) {
+$app->get('/{subject_type}/{subject_id}/{object_type}/{object_id}', 
+function(Application $app, $subject_type, $subject_id, $object_type, $object_id) use ($store) {
     
     $subject = ['type' => $subject_type, 'id' => $subject_id];
     $object = ['type' => $object_type, 'id' => $object_id];
 
     // obtain perms from storage, keyed by subject & object
-    // $perms = $storage->getPerms($subject, $object);
+    // $perms = $store->getPerms($subject, $object);
     $perms = new StdClass;
     $data = ['perms' => $perms, 'subject' => $subject, 'object' => $object]; 
     return $app->json($data);
--- src/Ace/Perm/Store.php
+++ src/Ace/Perm/Store.php
@@ -3,11 +3,12 @@
 use Ace\Perm\StoreInterface;
 use Ace\Perm\SubjectInterface;
 use Ace\Perm\ObjectInterface;
+use Ace\Perm\Perm;
 
 class Store implements StoreInterface
 {
     public function get(SubjectInterface $subject, ObjectInterface $object)
     {
-
+        return new Perm;
     }
 }
