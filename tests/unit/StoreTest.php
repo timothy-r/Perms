f620335cb6d25cf3e21d8cf0d14e32e45e263993
Tim Rodger <tim.rodger@gmail.com>
2014-02-14 12:33:10 +0000

    Adds test for retrieving multiple perms

--- tests/unit/StoreTest.php
+++ tests/unit/StoreTest.php
@@ -10,19 +10,41 @@ class StoreTest extends UnitTest
     
     protected $mock_db;
 
-    public function testGetReturnsAPermObject()
+    public function getPermValues()
+    {
+        return [
+            [['read']],
+            [['read','write']],
+            [['read','admin','read']],
+        ];
+    }
+
+    /**
+    * @dataProvider getPermValues
+    */
+    public function testGetReturnsAPermObject($values)
     {
         $object_id = '1s';
         $object_type = 'user';
         $subject_id = 'x';
         $subject_type = 'thread';
-        $perm_name = 'read';
+        
+        $rows = [];
+        foreach ($values as $value){
+        $rows[]= [
+            'subject_id' => $subject_id,
+            'subject_type' => $subject_type,
+            'object_id' => $object_id,
+            'object_type' => $object_type,
+            'value' => $value
+       ];
+       }
 
         $expected_sql = 
             "SELECT * FROM perm WHERE subject_id = ? AND subject_type = ? AND object_id = ? AND object_type = ?";
         
         $this->givenAMockDb();
-        $this->whenDbContains($expected_sql, $subject_id, $subject_type, $object_id, $object_type, $perm_name);
+        $this->whenDbContains($expected_sql, $rows);
         
         $this->givenAMockSubject($subject_id, $subject_type);
         $this->givenAMockObject($object_id, $object_type);
@@ -31,8 +53,9 @@ class StoreTest extends UnitTest
 
         $perm = $store->get($this->mock_subject, $this->mock_object);
         $this->assertInstanceOf('Ace\Perm\Perm', $perm);
-
-        $this->assertSame([$perm_name], $perm->allPerms());
+        
+        $expected = array_unique($values);
+        $this->assertSame($expected, $perm->allPerms());
     }
 
     public function testAddAddsAPermObject()
@@ -60,12 +83,12 @@ class StoreTest extends UnitTest
         $this->mock_db = $this->getMockBuilder('Doctrine\DBAL\Connection')->disableOriginalConstructor()->getMock();
     }
 
-    protected function whenDbContains($expected_sql, $subject_id, $subject_type, $object_id, $object_type, $perm)
+    protected function whenDbContains($expected_sql, $rows)
     {
         $this->mock_db->expects($this->once())
             ->method('fetchAll')
             ->with($expected_sql)
-            ->will($this->returnValue([['subject_id' => $subject_id, 'subject_type' => $subject_type, 'object_id' => $object_id, 'object_type' => $object_type, 'value' => $perm]]));
+            ->will($this->returnValue($rows));
     }
 
     protected function whenDbExpects($table, $subject_id, $subject_type, $object_id, $object_type, $perm)
