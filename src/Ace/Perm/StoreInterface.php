b1a4e6f4b74f91bc7e81970cbaeed7d5a093f340
Tim Rodger <tim.rodger@gmail.com>
2014-02-14 13:45:09 +0000

    Updates comments

--- src/Ace/Perm/StoreInterface.php
+++ src/Ace/Perm/StoreInterface.php
@@ -2,7 +2,11 @@
 
 use Ace\Perm\SubjectInterface;
 use Ace\Perm\ObjectInterface;
+use Ace\Perm\Perm;
 
+/**
+* @todo update to use the Perm class throughout
+*/
 interface StoreInterface
 {
     /**
@@ -15,4 +19,15 @@ interface StoreInterface
     * Adds this perm name to the Subject Object pair
     */
     public function add(SubjectInterface $subject, ObjectInterface $object, $value);
+
+    /**
+    * Removes perm from the Subject Object pair
+    * @todo this should remove all perm values for the Perm instance
+    */
+    public function remove(Perm $perm, $value);
+    
+    /**
+    * Updates the perm's values, both remove and add new ones
+    */
+    // public function update(Perms $perm); 
 }
