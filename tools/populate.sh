ce914f51c2e0cde174fed4fbe2adf9fb3d8a8d2a
Tim Rodger <tim.rodger@gmail.com>
2014-02-14 19:41:04 +0000

    Adds tool to populate db

---
+++ tools/populate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -X PUT http://perms.net/subject/user.101@accounts.us/object/article:34-we2@fp.com/admin
+curl -X PUT http://perms.net/subject/user.101@accounts.us/object/article:664-we2@fp.com/read
+curl -X PUT http://perms.net/subject/user.101@accounts.us/object/article:14-we2@fp.com/write
