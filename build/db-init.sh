14afaba41f056cc943512d094ac9eb429bf0afe2
Tim Rodger <tim.rodger@gmail.com>
2015-06-24 20:48:14 +0100

    Only create a single db file

--- build/db-init.sh
+++ build/db-init.sh
@@ -3,19 +3,7 @@
 mkdir ../data 2> /dev/null
 
 # deletes and remakes database files
-rm ../data/test.db 2> /dev/null
-rm ../data/dev.db 2> /dev/null
-rm ../data/prod.db 2> /dev/null
 rm ../data/perm.db 2> /dev/null
-
-touch ../data/test.db
-sqlite3 ../data/test.db < ./db-init.sql
-
-touch ../data/dev.db
-sqlite3 ../data/dev.db < ./db-init.sql
-
-touch ../data/prod.db
-sqlite3 ../data/prod.db < ./db-init.sql
 
 touch ../data/perm.db
 sqlite3 ../data/perm.db < ./db-init.sql
